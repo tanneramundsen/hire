9964eea882fc65b4eeb77af1d3ea60ac4de42f04
Madhu Rajmohan <43556104+mrajmoh1@users.noreply.github.com>
2020-02-22 17:51:11 -0500

    constructors

--- hïre/src/main/java/model/StaffMember.java
+++ hïre/src/main/java/model/StaffMember.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class StaffMember extends User {
     String name;
+    String jhed;
     List<Course> courses;
 
     public List<Applicant> getCAsForCourse(Course course) {
@@ -12,5 +13,48 @@ public class StaffMember extends User {
 
     public void updateCourses() {
 
+    }
+
+    public StaffMember(String name, String jhed, List<Course> courses) {
+        this.name = name;
+        this.jhed = jhed;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJhed() {
+        return jhed;
+    }
+
+    public void setJhed(String jhed) {
+        this.jhed = jhed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StaffMember)) return false;
+        StaffMember that = (StaffMember) o;
+        return Objects.equals(getJhed(), that.getJhed());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getJhed());
     }
 }
