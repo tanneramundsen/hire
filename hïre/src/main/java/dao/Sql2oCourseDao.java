5a8d3cfaf93ef99600b2df2f0f41c5c1568f6c1b
Madhu Rajmohan <43556104+mrajmoh1@users.noreply.github.com>
2020-02-22 18:48:27 -0500

    wefgjagf

--- hïre/src/main/java/dao/Sql2oCourseDao.java
+++ hïre/src/main/java/dao/Sql2oCourseDao.java
@@ -1,4 +1,31 @@
 package dao;
+import model.Course;
+import model.StaffMember;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-public class Sql2oCourseDao {
+import java.util.List;
+
+public class Sql2oCourseDao implements CourseDao {
+
+    public void add(Course course) throws RuntimeException{
+
+    }
+
+    public void update(Course course) throws RuntimeException{
+
+    }
+
+    public void delete(Course course) throws RuntimeException{
+
+    }
+
+    public List<Course> findCoursesByStaff(StaffMember staff){
+        return null;
+    }
+
+    public List<Course> findAll(){
+        return null;
+    }
 }
