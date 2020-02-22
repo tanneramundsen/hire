ab153db6d5528653c2f8cf85510e54b9d016f9d2
ChesterHuynh <chester.huynh924@gmail.com>
2020-02-22 18:49:30 -0500

    set up StaffMemberDao

--- hïre/src/main/java/dao/Sql2oStaffMemberDao.java
+++ hïre/src/main/java/dao/Sql2oStaffMemberDao.java
@@ -1,4 +1,47 @@
 package dao;
 
-public class Sql2oStaffMemberDao {
+import model.Course;
+import model.StaffMember;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oStaffMemberDao implements StaffMemberDao {
+
+    private Sql2o sql2o;
+
+    public Sql2oCourseDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(StaffMember staffMember) {
+
+        for(Course course: staffMember.getCourses()) {
+            try(Connection conn = sql2o.open()) {
+                String sql = "INSERT INTO StaffMembers(name, url) VALUES(:name, :url, :courseId);";
+                int id = (int) conn.createQuery(sql)
+                        .bind(staffMember)
+                        .executeUpdate()
+                        .getKey();
+            }
+        }
+    }
+
+    @Override
+    public void update(StaffMember staffMember) {
+
+    }
+
+    @Override
+    public void delete(StaffMember staffMember) {
+
+    }
+
+    @Override
+    public List<Course> getCoursesByStaff(StaffMember staffMember) {
+        return null;
+    }
 }
