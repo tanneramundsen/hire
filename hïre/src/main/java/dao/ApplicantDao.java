366064c990ec2b6c00357a8316067ecbf50394b3
Tanner Amundsen <tamunds1@jhu.edu>
2020-02-22 18:49:20 -0500

    Added applicantDao stuff

--- hïre/src/main/java/dao/ApplicantDao.java
+++ hïre/src/main/java/dao/ApplicantDao.java
@@ -1,4 +1,12 @@
 package dao;
 
+import model.Applicant;
+import exception.DaoException;
+import java.util.List;
+
 public interface ApplicantDao {
+
+    void add(Applicant applicant) throws RuntimeException;
+
+    List<Applicant> findAll();
 }
--- hïre/src/main/java/dao/Sql2oApplicantDao.java
+++ hïre/src/main/java/dao/Sql2oApplicantDao.java
@@ -1,4 +1,26 @@
 package dao;
 
-public class Sql2oApplicantDao {
+import exception.DaoException;
+import model.Applicant;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oApplicantDao implements ApplicantDao {
+
+    private Sql2o sql2o;
+
+    public Sql2oApplicantDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public List<Applicant> findAll() {
+        return null;
+    }
+
+    @Override
+    public void add(Applicant applicant) throws RuntimeException {
+
+    }
 }
