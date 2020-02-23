c25d6edb13335133ebde363a57ee554d1b569f84
Madhu Rajmohan <43556104+mrajmoh1@users.noreply.github.com>
2020-02-23 14:44:51 -0500

    course dao

--- hïre/src/main/java/dao/Sql2oCourseDao.java
+++ hïre/src/main/java/dao/Sql2oCourseDao.java
@@ -9,8 +9,18 @@ import java.util.List;
 
 public class Sql2oCourseDao implements CourseDao {
 
-    public void add(Course course) throws RuntimeException{
+    private Sql2o sql2o;
 
+    public void add(Course course) throws RuntimeException{
+        try (Connection conn = sql2o.open()) {
+            String sql = "INSERT INTO Courses(name, url) VALUES(:name, :url);";
+            int id = (int) conn.createQuery(sql)
+                    .bind(course)
+                    .executeUpdate()
+                    .getKey();
+        } catch (Sql2oException ex) {
+            throw new RuntimeException("Unable to add the course", ex);
+        }
     }
 
     public void update(Course course) throws RuntimeException{
@@ -22,10 +32,16 @@ public class Sql2oCourseDao implements CourseDao {
     }
 
     public List<Course> findCoursesByStaff(StaffMember staff){
-        return null;
+        try (Connection conn = sql2o.open()) {
+            String sql = "SELECT * FROM Courses WHERE ;";
+            return conn.createQuery(sql).executeAndFetch(Course.class);
+        }
     }
 
     public List<Course> findAll(){
-        return null;
+        try (Connection conn = sql2o.open()) {
+            String sql = "SELECT * FROM Courses;";
+            return conn.createQuery(sql).executeAndFetch(Course.class);
+        }
     }
 }
