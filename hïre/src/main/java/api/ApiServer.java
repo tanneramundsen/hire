27bbc7e96fbba5989b253fad93edd78bc13e835e
Jen <linjennifer106@gmail.com>
2020-02-23 16:30:36 -0500

    original page

--- hïre/src/main/java/api/ApiServer.java
+++ hïre/src/main/java/api/ApiServer.java
@@ -1,6 +1,7 @@
 package api;
 
 import com.google.gson.Gson;
+import dao.ApplicantDao;
 import dao.CourseDao;
 import dao.DaoFactory;
 import dao.DaoUtil;
@@ -23,12 +24,6 @@ public final class ApiServer {
 
     public static void start() {
         app = startJavalin();
-
-        // runs after every request (even if an exception occurred)
-        app.after(ctx -> {
-            // run after all requests
-            ctx.contentType("application/json");
-        });
     }
 
     public static void stop() {
@@ -44,63 +39,5 @@ public final class ApiServer {
             config.addStaticFiles("/public");
         }).start(PORT);
     }
-/*
-    private static void getHomepage() {
-        app.get("/", ctx -> ctx.result("CourseReVU RESTful API"));
-    }
-
-    private static void getCourses(CourseDao courseDao) {
-        // handle HTTP Get request to retrieve all courses
-        app.get("/courses", ctx -> {
-            List<Course> courses = courseDao.findAll();
-            ctx.json(courses);
-            ctx.status(200); // everything ok!
-        });
-    }
-
-    private static void postCourses(CourseDao courseDao) {
-        // client adds a course through HTTP POST request
-        app.post("/courses", ctx -> {
-            Course course = ctx.bodyAsClass(Course.class);
-            try {
-                courseDao.add(course);
-                ctx.status(201); // created successfully
-                ctx.json(course);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500); // server internal error
-            }
-        });
-    }
-
-    private static void getReviewsForCourse(ReviewDao reviewDao) {
-        // handle HTTP Get request to retrieve all reviews for a course
-        // TODO: DONE
-        app.get("/courses/:id/reviews", ctx -> {
-            int curCourse = Integer.parseInt(ctx.pathParam("id"));
-            List<Review> reviews = reviewDao.findByCourseId(curCourse);
-            ctx.json(reviews);
-            ctx.status(200); // everything ok!
-        });
-    }
-
-    private static void postReviewForCourse(ReviewDao reviewDao) {
-        // client adds a review for a course (given its id) using HTTP POST request
-        // TODO: DONE
-        app.post("/courses/:id/reviews", ctx -> {
-            int curCourse = Integer.parseInt(ctx.pathParam("id"));
-            Review review = ctx.bodyAsClass(Review.class);
-            if (review.getCourseId() != curCourse) {
-                throw new ApiError("Review is not part of this course", 400);
-            }
-            try {
-                reviewDao.add(review);
-                ctx.status(201); // created successfully
-                ctx.json(review);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500); // server internal error
-            }
-        });
-    }
-    */
 
 }
