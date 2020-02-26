94dd0b9d3adba1bc12769641e906f47412713de7
Madhu Rajmohan <43556104+mrajmoh1@users.noreply.github.com>
2020-02-25 19:40:37 -0500

    stuff

--- hïre/src/main/java/api/ApiServer.java
+++ hïre/src/main/java/api/ApiServer.java
@@ -7,9 +7,13 @@ import dao.CourseDao;
 import dao.StaffMemberDao;
 import dao.DaoFactory;
 import dao.DaoUtil;
+import exception.ApiError;
+import exception.DaoException;
 import io.javalin.Javalin;
 import io.javalin.plugin.json.JavalinJson;
 import model.Course;
+import model.StaffMember;
+import model.Applicant;
 
 
 import java.util.HashMap;
@@ -47,36 +51,12 @@ public final class ApiServer {
         // routing
         postCourses(courseDao);
         getCourses(courseDao);
-    }
-    private static void postCourses(CourseDao courseDao) {
-        // client adds a course through HTTP POST request
-        app.post("/courses", ctx -> {
-            Course course = ctx.bodyAsClass(Course.class);
-            try {
-                courseDao.add(course);
-                ctx.status(201); // created successfully
-                ctx.json(course);
-            } catch (RuntimeException ex) {
-                throw new RuntimeException(ex.getMessage(), ex); // FIX!!!!
-            }
-        });
-    }
-
-    private static void getCourses(CourseDao courseDao) {
-        // handle HTTP Get request to retrieve all courses
-        app.get("/courses", ctx -> {
-            List<Course> courses = courseDao.findAll();
-            ctx.json(courses);
-            ctx.status(200); // everything ok!
-        });
-    }
-        /**
-         // Routing
-        getHomepage();
-        getCourses(courseDao);
-        postCourses(courseDao);
-        getReviewsForCourse(reviewDao);
-        postReviewForCourse(reviewDao);
+        updateCourses(courseDao);
+        deleteCourses(courseDao);
+        postApplicants(applicantDao);
+        getApplicants(applicantDao);
+        updateApplicants(applicantDao);
+        deleteApplicants(applicantDao);
 
         // Handle exceptions
         app.exception(ApiError.class, (exception, ctx) -> {
@@ -94,7 +74,107 @@ public final class ApiServer {
             ctx.contentType("application/json");
         });
     }
-         */
+    private static void postCourses(CourseDao courseDao) {
+        // client adds a course through HTTP POST request
+        app.post("/courses", ctx -> {
+            Course course = ctx.bodyAsClass(Course.class);
+            try {
+                courseDao.add(course);
+                ctx.status(201); // created successfully
+                ctx.json(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
+
+    private static void updateCourses(CourseDao courseDao) {
+        // client updates a course through HTTP POST request
+        app.post("/courses", ctx -> {
+            Course course = ctx.bodyAsClass(Course.class);
+            try {
+                courseDao.update(course);
+                ctx.status(201); // created successfully
+                ctx.json(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
+
+    private static void getCourses(CourseDao courseDao) {
+        // handle HTTP Get request to retrieve all courses
+        app.get("/courses", ctx -> {
+            List<Course> courses = courseDao.findAll();
+            ctx.json(courses);
+            ctx.status(200); // everything ok!
+        });
+    }
+
+    private static void deleteCourses(CourseDao courseDao) {
+        // client delete a course through HTTP POST request
+        app.delete("/courses", ctx -> {
+            Course course = ctx.bodyAsClass(Course.class);
+            try {
+                courseDao.delete(course);
+                ctx.status(201); // created successfully
+                ctx.json(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
+
+    private static void postApplicants(ApplicantDao applicantDao) {
+        // client adds a course through HTTP POST request
+        app.post("/applicants", ctx -> {
+            Applicant applicant = ctx.bodyAsClass(Applicant.class);
+            try {
+                applicantDao.add(applicant);
+                ctx.status(201); // created successfully
+                ctx.json(applicant);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
+
+    private static void updateApplicants(ApplicantDao applicantDao) {
+        // client updates a course through HTTP POST request
+        app.post("/applicants", ctx -> {
+            Applicant applicant = ctx.bodyAsClass(Applicant.class);
+            try {
+                applicantDao.update(applicant);
+                ctx.status(201); // created successfully
+                ctx.json(applicant);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
+
+    private static void getApplicants(ApplicantDao applicantDao) {
+        // handle HTTP Get request to retrieve all courses
+        app.get("/applicants", ctx -> {
+            List<Applicant> applicants = applicantDao.findAll();
+            ctx.json(applicants);
+            ctx.status(200); // everything ok!
+        });
+    }
+
+    private static void deleteApplicants(ApplicantDao applicantDao) {
+        // client delete a course through HTTP POST request
+        app.delete("/applicants", ctx -> {
+            Applicant applicant = ctx.bodyAsClass(Applicant.class);
+            try {
+                applicantDao.delete(applicant);
+                ctx.status(201); // created successfully
+                ctx.json(applicant);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+    }
 
     public static void stop() {
         app.stop();
