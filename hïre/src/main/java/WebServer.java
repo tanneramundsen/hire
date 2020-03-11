cbfb678b755b453d7d20b33a929441b87a79495d
Madhu Rajmohan <43556104+mrajmoh1@users.noreply.github.com>
2020-03-11 15:08:18 -0400

    updates course info

--- hïre/src/main/java/WebServer.java
+++ hïre/src/main/java/WebServer.java
@@ -109,19 +109,17 @@ public class WebServer {
         get("/:id/courseinfo", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
 
+            String jhed = request.cookie("jhed");
             int courseId = Integer.parseInt(request.params(":id"));
             model.put("courseID", courseId);
-
-            // String courseName = request.cookie("name");
-            Course course = courseDao.read(courseId);
-            String courseName = course.getName();
-            String courseNumber = course.getCourseNumber();
-            List<Applicant> interestedApplicants = course.getInterestedApplicants();
-            List<Applicant> hiredApplicants = course.getHiredApplicants();
-            List<StaffMember> instructors = course.getInstructors();
+            String name = courseDao.read(courseId).getName();
+            String courseNumber = courseDao.read(courseId).getCourseNumber();
+            List<Applicant> interestedApplicants = courseDao.read(courseId).getInterestedApplicants();
+            List<Applicant> hiredApplicants = courseDao.read(courseId).getHiredApplicants();
+            List<StaffMember> instructors = courseDao.read(courseId).getInstructors();
 
             /* later can put in semester */
-            model.put("courseName", courseName);
+            model.put("name", name);
             model.put("courseNumber", courseNumber);
             model.put("interestedApplicants", interestedApplicants);
             model.put("hiredApplicants", hiredApplicants);
